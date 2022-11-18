9945240fe86043ae73be52cf42c8967e3d4f8ed2
Alexander Cheprasov <cheprasov_sashuta@mail.ru>
2022-11-18 10:40:50 +0300

    Сырая версия

---
+++ funcLib.cpp
@@ -0,0 +1,6 @@
+#include "funcLib.h"
+
+int isNumber(char a){
+    if ('0'<=a && a<='9')return 1;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "funcLib.h"
+
+using namespace std;
+
+int main(){
+    char symb;
+    cin>>symb;
+    cout<<isNumber(symb);
+    return 0;
+}
